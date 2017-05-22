2410efb07be1702d042f8abf305b10b51e5f9b92
Rex Hardin <masterrex@outlook.com>
2017-05-22 14:30:40 -0500

    Updated for pylint/pep8 compliance

--- csgoscript.py
+++ csgoscript.py
@@ -1,32 +1,52 @@
-import feedparser # https://pythonhosted.org/feedparser/introduction.html
+"""
+CSGo
+"""
 
 # 126 chars max per message
 import os
 from os.path import expanduser
+import feedparser # https://pythonhosted.org/feedparser/introduction.html
 
-CONFIG_PATH = os.path.join(expanduser("~"), ".steam/steam/userdata/11713211/730/local/cfg/")
+CONFIG_PATH = os.path.join(expanduser("~"),
+                            ".steam/steam/userdata/11713211/730/local/cfg/")
 
 
 class CSGONewsConfig(object):
+    """
+    Simple Object representing a config to sync with subreddit
+    """
     def __init__(self, subreddit):
         self.subreddit = subreddit
-        self.keybinds  =  [
-            "kp_slash",  "kp_multiply", "kp_minus",
+        self.keybinds = [
+            "kp_slash", "kp_multiply", "kp_minus",
             "kp_home", "kp_uparrow", "kp_pgup", "kp_plus",
             "kp_leftarrow", "kp_5", "kp_rightarrow",
             "kp_end", "kp_downarrow", "kp_pgdn", "kp_enter",
-            "kp_ins", "kp_del"
-
-        ]
+            "kp_ins", "kp_del"]
+        self.feed = None
     def write_cfg(self):
-        with open(os.path.join(CONFIG_PATH, config.subreddit + '.cfg'), 'w') as cfg:
+        """
+        Write config to filesystem
+        """
+        cfg_file = os.path.join(CONFIG_PATH, self.subreddit + '.cfg')
+        with open(cfg_file, 'w') as cfg:
             for i in range(0, len(self.keybinds)):
-                cfg.write('\nunbind "{0}"\nbind "{0}" "say #{1}: {2}"'.format(self.keybinds[i], i, self.feed['entries'][i]['title'].encode('utf-8')))
+                cfg.write('\nunbind "{0}"\nbind "{0}" "say #{1}: {2}"'.format(
+                    self.keybinds[i],
+                    i,
+                    self.feed['entries'][i]['title'].encode('utf-8')))
     def get_rss_feed(self):
-        self.feed = feedparser.parse('http://reddit.com/r/%s.rss' % self.subreddit)
+        """
+        Returns rss feed data as a dict
+        """
+        reddit_rss_feed = 'http://reddit.com/r/%s.rss' % self.subreddit
+        self.feed = feedparser.parse(reddit_rss_feed)
 
-configs = [ CSGONewsConfig('the_donald'), CSGONewsConfig('hillaryforprison'), CSGONewsConfig('sorosforprison') ]
+CONFIGS = [
+    CSGONewsConfig('the_donald'),
+    CSGONewsConfig('hillaryforprison'),
+    CSGONewsConfig('sorosforprison')]
 
-for config in configs:
-    config.get_rss_feed()
-    config.write_cfg()
+for c in CONFIGS:
+    c.get_rss_feed()
+    c.write_cfg()
